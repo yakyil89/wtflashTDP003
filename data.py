817894897ff237755d1ad812d28ffdab19a374b9
Anton <bantonlindgren@gmail.com>
2012-09-24 16:42:34 +0200

    Created get_techniques() in data.py

--- data.py
+++ data.py
@@ -12,17 +12,27 @@ def load(name):
         return
 
 def get_project_count(db):
+    """Returns the length of the list as an int."""
     return len(db)
 
 def get_project(db, id):
+    """Returns the dictionary containing the project with a given id."""
     for i in range(len(db)):
         if db[i]["project_no"] == id:
             return db[i]
     return
 
-#for i in load('data.json'):
-#    print i["project_no"]
+def get_techniques(db):
+    """Returns a list of all techniques used, ordered alphabetically."""
+    the_set = set()
+    for i in range(len(db)):
+        for t in range(len(db[i]["techniques_used"])):
+            the_set.add(db[i]["techniques_used"][t])
+    lis = list(the_set)
+    lis.sort()
+    return lis
 
-print get_project(load('data.json'), 2)
-print get_project_count(load('data.json'))
-print load('data.json')
+print get_techniques(load('data.json'))
+get_project(load('data.json'), 2)
+get_project_count(load('data.json'))
+load('data.json')
