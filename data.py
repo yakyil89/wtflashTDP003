6e839b0943004705f2143fd8a2dcdce14ae02981
Anton <bantonlindgren@gmail.com>
2012-09-24 16:04:01 +0200

    Created get_project()

--- data.py
+++ data.py
@@ -14,5 +14,15 @@ def load(name):
 def get_project_count(db):
     return len(db)
 
+def get_project(db, id):
+    for i in range(len(db)):
+        if db[i]["project_no"] == id:
+            return db[i]
+    return
+
+#for i in load('data.json'):
+#    print i["project_no"]
+
+print get_project(load('data.json'), 2)
 print get_project_count(load('data.json'))
 print load('data.json')
