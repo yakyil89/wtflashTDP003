e5f6490ec61ff9035281cd3a39c9ad5cf990ff6b
Anton <bantonlindgren@gmail.com>
2012-09-24 15:31:36 +0200

    Fixed load() and created get_project_count()

--- data.py
+++ data.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import json
 
 def load(name):
     """Loads a json file and returns the result."""
-    with open(name, 'r') as json_file:
-        data = json.load(json_file)
-    return data
+    try: 
+        with open(name, 'r') as json_file:
+            data = json.load(json_file)
+            return data
+    except:
+        return
 
+def get_project_count(db):
+    return len(db)
 
+print get_project_count(load('data.json'))
 print load('data.json')
