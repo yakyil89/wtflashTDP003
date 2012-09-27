fc68ba4d8022bbf8cd9c8f256ba47502f3448480
Anton <bantonlindgren@gmail.com>
2012-09-27 18:47:31 +0200

    Created the search() function.

--- data.py
+++ data.py
@@ -59,3 +59,91 @@ def get_technique_stats(db):
             # Adds the list to the dictionary, with the technique as key.
             dic[t] = lis
     return dic
+
+def free_search(db, search):
+    """Free search."""
+    lis = []
+    if search == None:
+        lis = db
+        return lis
+    if isinstance(search, unicode):
+        search = search.upper()
+        for i in range(len(db)):
+            for p in db[i]:
+                # Without this line the code will fail when it comes to an int.
+                if isinstance(db[i][p], (unicode)):
+                    project_upper = db[i][p].upper()
+                    if search in project_upper:
+                        lis.append(db[i])
+                        break
+    return lis
+
+def free_search_limited(db, search, search_fields):
+    """Free search with limited search fields."""
+    lis = []
+    if search == None:
+        lis = db
+        return lis
+    if isinstance(search, (unicode, str)):
+        search = search.upper()
+        for i in range(len(db)):
+            for p in search_fields:
+                # Without this line the code will fail when it comes to an int.
+                if isinstance(db[i][p], (unicode, int, str)):
+                    project_upper = unicode(db[i][p])
+                    try:
+                        search = unicode(search, 'utf-8')
+                    except: pass
+                    search = search.upper()
+                    if search in project_upper:
+                        lis.append(db[i])
+                        break
+    return lis
+
+def search(db, sort_by=u'start_date', sort_order=u'desc', techniques=None, search=None,
+           search_fields=None):
+    """Given a set of parameters, this function searches through the matching project.
+    Returns a list of the projects and their attributes."""
+
+    if search_fields == '':
+        return
+    if search == '':
+        return
+
+    lis = []
+    if search_fields == None:
+        lis = free_search(db, search)
+    else:
+        lis = free_search_limited(db, search, search_fields)
+    if lis == '':
+        return
+        
+
+    tech_lis = []
+    
+    # Sorts out the techniques.
+    if techniques == None:
+        tech_lis = lis
+    else:
+        if techniques == []:
+            techniques = get_techniques(db)
+        for i in range(len(lis)):
+            for t in range(len(techniques)):
+                if techniques[t] in lis[i][u'techniques_used']:
+                    tech_lis.append(lis[i])
+                    break
+    
+    # Order the list.
+    sort_key = []
+    sorted_lis = []
+    for i in range(len(tech_lis)):
+        sort_key.append(tech_lis[i][sort_by])
+    sort_key.sort()
+    if sort_order == u'desc':
+        sort_key.reverse()
+
+    for k in range(len(sort_key)):
+        for l in range(len(tech_lis)):
+            if tech_lis[l][sort_by] == sort_key[k]:
+                sorted_lis.append(tech_lis[l])
+    return sorted_lis
