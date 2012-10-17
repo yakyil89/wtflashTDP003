d94b1d92c4037b9ff3510ee60b6417f4087c7060
Anton <anton@anton-OptiPlex-740-Enhanced.(none)>
2012-10-17 11:15:10 +0200

    tweaks

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 # -*- coding-utf-8 -*-
+"""Server module that makes the website able to run, also redirects the
+links on the pages to the right pages."""
+
 from flask import Flask, request, url_for, render_template
 import data
+
+# Database (data.json).
 db = data.load('data.json')
 
+# List of all used techniques.
 all_techniques = data.get_techniques(db)
 
+# Variabel to run the website.
 app = Flask(__name__)
 app.debug = True
 
@@ -25,17 +32,20 @@ def log(page, data=None):
 
 @app.route('/')
 def index():
+    """Redirects to index.html"""
     log('index.html')
     return render_template('index.html', data=data, db=db)
 
 @app.route('/list/')
 def list():
+    """Redirects to list.html"""
     log('list.html')
     sorted_db = data.order(db)
     return render_template('list.html', data=data, db=sorted_db)
 
 @app.route('/project/<int:project_id>/')
 def project(project_id):
+    """Redirects to project.html and sends the project with the ID given in the URL"""
     log('project.html', ('id: ' + str(project_id)))
     project = data.get_project(db, project_id)
     if not project_id in data.get_ids(db):
@@ -44,13 +54,14 @@ def project(project_id):
 
 @app.route('/techniques/')
 def techniques():
+    """Redirects to techniques.html"""
     log('techniques.html')
     techniques = data.get_techniques(db)
     return render_template('techniques.html', all_techniques=all_techniques, data=data, techniques=techniques, db=db)
 
 @app.route('/search/', methods=['POST'])
 def search():
-
+    """Redirects to search.html and sends a list with projects that's given from the search form"""
     key = request.form['key'].encode('UTF-8').upper()
     if key == '':
         key = None
@@ -75,10 +86,12 @@ def search():
     
 @app.errorhandler(404)
 def page_not_found(e):
+    """Handles 404-errors"""
     return render_template('404.html', data=data, db=db), 404
 
 @app.errorhandler(500)
 def page_not_found(e):
+    """Handles 500-errors"""
     return render_template('500.html', data=data, db=db), 500
 
 if __name__ == '__main__':
